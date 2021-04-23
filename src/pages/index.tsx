412e6a6ec54cfd99bcc0c6b78f33220ee927705f
Nipodemos <nipodemos13@gmail.com>
2021-04-23 00:35:18 -0300

    parcial da aula 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,73 @@
-export default function Home() {
-  return <h1>Index</h1>;
+/* eslint-disable camelcase */
+import { GetStaticProps } from 'next';
+import ptbr, { format, parseISO } from 'date-fns';
+
+import api from '../services/api';
+import convertDurationToTimeString from '../utils/convert_duration_to_timestring';
+
+type HomeProps = {
+  episodes: Array<Episode>;
+};
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  publishedAt: string;
+  thumbnail: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
+};
+
+export default function Home({ episodes }: HomeProps) {
+  return <p>{JSON.stringify(episodes)}</p>;
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+type IRequest = {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+  thumbnail: string;
+  description: string;
+  file: {
+    url: string;
+    type: string;
+    duration: number;
+  };
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get<IRequest[]>('/episodes', {
+    params: {
+      limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  });
+
+  const episodes = data.map((episode) => ({
+    id: episode.id,
+    title: episode.title,
+    members: episode.members,
+    publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+      locale: ptbr,
+    }),
+    thumbnail: episode.thumbnail,
+    duration: Number(episode.file.duration),
+    durationAsString: convertDurationToTimeString(
+      Number(episode.file.duration),
+    ),
+    description: episode.description,
+    url: episode.file.url,
+  }));
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   };
-}
+};
